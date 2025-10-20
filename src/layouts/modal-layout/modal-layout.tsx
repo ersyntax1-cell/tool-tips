2c42507333c02ab38d1723421131407f7cccca79
Eric <ersyntax1>
2025-10-20 20:38:04 +0400

    add domain fetch

--- src/layouts/modal-layout/modal-layout.tsx
+++ src/layouts/modal-layout/modal-layout.tsx
@@ -1,8 +1,26 @@
+import { useEffect } from 'react'
 import ActionBar from '../../components/action-bar/action-bar'
 import GlobalPicker from '../../components/global-picker/global-picker'
 import ToolTipOverlay from '../../components/tooltip-overlay/tooltip-overlay'
 
 export default function ModalLayout() {
+
+  async function fetchDomain() {
+    const domain = window.location.hostname;
+
+    fetch('http://localhost:3000/domain', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({domain})
+    })
+    .then((res) => res.json())
+    .catch(console.error);
+  }
+
+  useEffect(() => {
+    fetchDomain();
+  }, []);
+
   return (
     <>
         <ActionBar />
