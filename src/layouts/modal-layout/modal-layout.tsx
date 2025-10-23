6e1cc7864d8b2a604332b28a9a07ec4d235f6e4f
Eric <ersyntax1>
2025-10-23 17:34:05 +0400

    commit

--- src/layouts/modal-layout/modal-layout.tsx
+++ src/layouts/modal-layout/modal-layout.tsx
@@ -1,12 +1,43 @@
-import RedirectComponent from '../../components/redirect-component/redirect-component'
+// import RedirectComponent from '../../components/redirect-component/redirect-component'
 // import { AuthProvider } from '../../context/auth-provider/auth-provider'
 
+import { useEffect } from "react";
+import ActionBar from "../../components/action-bar/action-bar";
+import GlobalPicker from "../../components/global-picker/global-picker";
+import ToolTipOverlay from "../../components/tooltip-overlay/tooltip-overlay";
+
 export default function ModalLayout() {
+
+  async function fetchDomain() {
+    const domain = window.location.hostname;
+    const token = localStorage.getItem('token');
+
+    if (!token) return;
+
+    fetch('http://localhost:3000/domain', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({ domain })
+    })
+      .then(res => res.json())
+      .catch(console.error);
+  }
+
+  useEffect(() => {  
+      fetchDomain();
+    }, []);
+
   return (
     <>
       {/* <AuthProvider> */}
-        <RedirectComponent />
+      {/* <RedirectComponent /> */}
       {/* </AuthProvider> */}
+      <ActionBar />
+      <GlobalPicker />
+      <ToolTipOverlay />
     </>
   )
 }
